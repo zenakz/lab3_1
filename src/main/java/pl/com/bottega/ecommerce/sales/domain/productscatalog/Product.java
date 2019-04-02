13d6372ec645c4e840f2df38fd9a89b0e7c294f3
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2019-04-02 11:27:11 +0200

    making constructor public

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/Product.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/Product.java
@@ -2,47 +2,42 @@ package pl.com.bottega.ecommerce.sales.domain.productscatalog;
 
 import java.util.Date;
 
-
-
 import pl.com.bottega.ddd.support.domain.BaseAggregateRoot;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
-public class Product extends BaseAggregateRoot{
-	private Money price;
-	
-	private String name;
-	
+public class Product extends BaseAggregateRoot {
 
-	private ProductType productType;
-	
+    private Money price;
 
-	private Product(){}
-	
-	Product(Id aggregateId, Money price, String name, ProductType productType){
-		this.id = aggregateId;
-		this.price = price;
-		this.name = name;
-		this.productType = productType;
-	}
-	
-	public boolean isAvailable(){		
-		return ! isRemoved();
-	}
-	
-	public Money getPrice() {
-		return price;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public ProductType getProductType() {
-		return productType;
-	}
-	
-	public ProductData generateSnapshot(){
-		return new ProductData(getId(), price, name, productType, new Date());
-	}
+    private String name;
+
+    private ProductType productType;
+
+    public Product(Id aggregateId, Money price, String name, ProductType productType) {
+        this.id = aggregateId;
+        this.price = price;
+        this.name = name;
+        this.productType = productType;
+    }
+
+    public boolean isAvailable() {
+        return !isRemoved();
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ProductType getProductType() {
+        return productType;
+    }
+
+    public ProductData generateSnapshot() {
+        return new ProductData(getId(), price, name, productType, new Date());
+    }
 }
