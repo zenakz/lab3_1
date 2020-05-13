d8b855446610a88a01edc499af093a205dfc9ae3
Zientak.David <Zenakz@github.com>
2020-05-13 13:38:28 +0200

    added dependencies and BookKeeperTest class

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,16 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookKeeperTest {
+
+}
