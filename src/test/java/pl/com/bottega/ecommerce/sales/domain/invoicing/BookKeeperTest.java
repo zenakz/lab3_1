7721605933fd6752f2f265da628a6889208c1d0d
Zientak.David <Zenakz@github.com>
2020-05-13 13:44:26 +0200

    added first state test

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -4,13 +4,40 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class BookKeeperTest {
+    private static BookKeeper bookKeeper;
+    private static Money money;
+    private static Product product;
+    private static ClientData clientData;
+    private static InvoiceRequest invoiceRequest;
+
+    @Mock
+    private static TaxPolicy taxPolicy=mock(TaxPolicy.class);
+    @BeforeAll
+    public static void start(){
+        bookKeeper = new BookKeeper(new InvoiceFactory());
+        clientData = new ClientData(Id.generate(), "");
+        invoiceRequest = new InvoiceRequest(clientData);
+        money = new Money(1, Money.DEFAULT_CURRENCY);
+        product = new Product(Id.generate(), money, "product", ProductType.STANDARD);
+        Mockito.when(taxPolicy.calculateTax(Mockito.any(), Mockito.any())).thenReturn(new Tax(Money.ZERO, ""));
+    }
+
+    @Test
+    void oneItemRequestShouldReturnOneItem(){
+        RequestItem requestItem = new RequestItem(product.generateSnapshot(), 1, money);
+        invoiceRequest.add(requestItem);
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        assertEquals(1, invoice.getItems().size());
+    }
 
 }
